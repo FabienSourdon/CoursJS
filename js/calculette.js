cf22b29235e100518793f569b459acab0026b7ab
Fabien Sourdon <fabien.sourdon@gmail.com>
2020-04-02 00:08:36 +0200

    test

--- js/calculette.js
+++ js/calculette.js
@@ -27,10 +27,16 @@ function addNbr(a)
 
 function addOp(a)
 {
-    document.getElementById('saisie').innerHTML += a;
-    operateur = a;
-    console.log(operateur);
-
+    if (operateur == ''){
+        document.getElementById('saisie').innerHTML += a;
+        operateur = a;
+        console.log(operateur);
+    }
+    else{
+        document.getElementById('saisie').innerHTML = ch1 + a;
+        operateur = a;
+        console.log(operateur);
+    }
 }
 
 function retour()
@@ -55,32 +61,33 @@ function annul()
     destina = origine.slice(0, -ch2.length);
     document.getElementById('saisie').innerHTML = destina;
     ch2 = '';
-    console.log("destina:", destina);
+    console.log('ch1:', ch1, 'ch2:', ch2, 'opérateur :', operateur);
 }
 
 function calc(){
+
     switch (operateur)
     {
         case '+':
             var calcul = parseFloat(ch1) + parseFloat(ch2);
-            document.getElementById('saisie').innerHTML = calcul;
             console.log(calcul);
             break;
         case '-':
             var calcul = parseFloat(ch1) - parseFloat(ch2);
-            document.getElementById('saisie').innerHTML = calcul;
             console.log(calcul);
             break;
         case '*':
             var calcul = parseFloat(ch1) * parseFloat(ch2);
-            document.getElementById('saisie').innerHTML = calcul;
             console.log(calcul);
             break;
         case '/':
             var calcul = parseFloat(ch1) / parseFloat(ch2);
-            document.getElementById('saisie').innerHTML = calcul;
             console.log(calcul);
             break;
     }
-}
-;
+
+    ch1 = calcul;
+    document.getElementById('saisie').innerHTML = ch1;
+    operateur = '';
+    ch2 = '';
+};
